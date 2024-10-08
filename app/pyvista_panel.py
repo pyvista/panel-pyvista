eae220800f30eb9f50b529d3891fa31abed0fff6
Alex Kaszynski <akascap@gmail.com>
2024-10-08 15:27:14 -0600

    use panel vtk

--- app/pyvista_panel.py
+++ app/pyvista_panel.py
@@ -1,44 +1,24 @@
 import panel as pn
 import pyvista as pv
-from pyvista.plotting import Plotter
-from pyvista.trame.jupyter import elegantly_launch
 from pyvista import demos as pv_demos
 
 from common import GlobalStateMixin
 
-elegantly_launch(port=5090)
-# elegantly_launch(port=5090, wslink_backend="tornado")  # doesn't work
+pn.extension("vtk")
 
 pv.set_plot_theme("document")
 pv.global_theme.show_scalar_bar = False
 
 
-def show_panel(
-    plotter: Plotter, jupyter_backend: str = "server", **kwargs
-) -> pn.pane.HTML:
-    def panel_handler(viewer, src, **_kwargs):
-        html = f'<iframe src="{src}" class="pyvista" style="width: 100%; height: 100%;"></iframe>'
-        return pn.pane.HTML(html, sizing_mode="stretch_both")
-
-    kwargs.setdefault("jupyter_kwargs", {})
-    kwargs["jupyter_kwargs"]["handler"] = panel_handler
-
-    # Always return the viewer to access the Panel object
-    kwargs["return_viewer"] = True
-
-    return plotter.show(
-        jupyter_backend=jupyter_backend,
-        **kwargs,
-    )
+plotter = pv.Plotter()  # we define a pyvista plotter
+plotter.background_color = (0.1, 0.2, 0.4)
+# we create a `VTK` panel around the render window
+pn.panel(plotter.ren_win, width=500, height=500)
 
 
 class PyVistaPlotter(GlobalStateMixin, pn.viewable.Viewer):
-    def __init__(self, jupyter_backend: str = "server", **kwargs):
-        super().__init__(**kwargs)
-        self._plotter = Plotter(off_screen=True, notebook=True)
-        self._plotter.add_axes()
-        self._plotter_pane = show_panel(self.plotter, jupyter_backend=jupyter_backend)
-        self._plotter.camera_set = False
+    def __init__(self):
+        self._plotter = pv.Plotter()  # we define a pyvista plotter
 
         pl = pv_demos.plot_logo(just_return_plotter=True)
         for actor in pl.actors.values():
@@ -46,12 +26,15 @@ class PyVistaPlotter(GlobalStateMixin, pn.viewable.Viewer):
 
         self._plotter.camera_position = "xy"
 
+        # Create a `VTK` panel around the render window
+        self._vtk_panel = pn.panel(self._plotter.ren_win, width=1000, height=500)
+
     @property
     def plotter(self):
         return self._plotter
 
     def __panel__(self):
-        return self._plotter_pane
+        return self._vtk_panel
 
     def add_mesh(self, mesh, **kwargs):
         self.plotter.add_mesh(mesh, **kwargs)
